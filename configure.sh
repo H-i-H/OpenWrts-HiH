ca0e3d50c3469b09c0d46a3cc213011290af0547
H-i-H <pingjunxiong@outlook.com>
2025-05-15 00:42:33 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 # Modify default system settings
 
-# 暂时回滚
-sed -i '/^#src-git luci https:\/\/github.com\/coolsnowwolf\/luci$/s/^#//' feeds.conf.default && sed -i '/^src-git luci https:\/\/github.com\/coolsnowwolf\/luci\.git;openwrt-23\.05$/s/^/#/' feeds.conf.default
-
-# 修改默认IP为192.168.10.1
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate 
-
+# nikki 需要配合Firewall4
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 # Hello World
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-
-# luci-theme-infinityfreedom
-echo 'src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' >>feeds.conf.default
-
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >> "feeds.conf.default"
 # passwall
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
-echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> feeds.conf.default
+echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 
-# iStore
-echo "src-git istore https://github.com/linkease/istore;main" >> feeds.conf.default
+
+# 修改默认主机名
+# sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings
+
+# 设置密码为空
+sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认IP为 11.11.11.100
+##sed -i 's/192.168.1.1/11.11.11.100/g' package/base-files/luci2/bin/config_generate
+
+# 加入编译者信息
+sed -i "s/OpenWrt /H-i-H build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+
+# 修改默认主题
+## 新版（需要使用JS版本主题，否则会进不去后台，提示"Unhandled exception during request dispatching"）
+# sed -i "s/luci-theme-bootstrap/luci-theme-design/g" feeds/luci/collections/luci-light/Makefile
+## 旧版
+# sed -i "s/luci-theme-bootstrap/luci-theme-design/g" feeds/luci/collections/luci/Makefile
 
 # 替换默认主题
-rm -rf package/lean/luci-theme-argon 
-git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
+# rm -rf package/lean/luci-theme-argon 
+# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon

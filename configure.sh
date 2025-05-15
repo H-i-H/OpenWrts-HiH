6444c38d500ae9e6a7bf3e09f8d6160907cc9081
H-i-H <pingjunxiong@outlook.com>
2025-05-15 20:37:48 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -11,13 +11,13 @@ echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-pa
 
 
 # 修改默认主机名
-# sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings
+sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings
 
 # 设置密码为空
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 修改默认IP为 11.11.11.100
-##sed -i 's/192.168.1.1/11.11.11.100/g' package/base-files/luci2/bin/config_generate
+sed -i 's/192.168.1.1/11.11.11.100/g' package/base-files/luci2/bin/config_generate
 
 # 加入编译者信息
 sed -i "s/OpenWrt /H-i-H build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings

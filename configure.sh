c87f17d7e6122ac51f609b712591d98d4c6ab65c
H-i-H <pingjunxiong@outlook.com>
2025-05-16 20:05:00 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -6,7 +6,7 @@ echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >>
 # Hello World
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >> "feeds.conf.default"
 # passwall
-echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
+# echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 # echo "src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2.git;main" >> "feeds.conf.default"
 
 

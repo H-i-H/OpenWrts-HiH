7a11b71d54cd3cf2598e221f5a7affd9becd1122
H-i-H <pingjunxiong@outlook.com>
2025-05-20 18:58:54 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -9,12 +9,3 @@ sed -i 's/192.168.1.1/11.11.11.100/g' package/base-files/luci2/bin/config_genera
 
 # 加入编译者信息
 sed -i "s/OpenWrt /HiH build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
-
-# 替换默认主题
-# rm -rf package/lean/luci-theme-argon 
-# git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git  package/lean/luci-theme-argon
-rm -rf package/lean/luci-theme-design
-git clone -b js https://github.com/gngpp/luci-theme-design.git  package/lean/luci-theme-design
-
-# 修改默认主题新版（需要使用JS版本主题，否则会进不去后台，提示"Unhandled exception during request dispatching"）
-# sed -i "s/luci-theme-bootstrap/luci-theme-design/g" feeds/luci/collections/luci-light/Makefile

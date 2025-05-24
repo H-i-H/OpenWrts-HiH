57c3a69d35b69a8d8f211c467a38a6da4647edc4
H-i-H <pingjunxiong@outlook.com>
2025-05-25 00:45:32 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,13 +1,13 @@
 # Theme
 # luci-theme-alpha
-git clone --depth 1 https://github.com/derisamedia/luci-theme-alpha.git  package/feeds/luci/luci-theme-alpha
+git clone https://github.com/derisamedia/luci-theme-alpha.git  package/feeds/luci/luci-theme-alpha
 
 # Mentohust（锐捷网络）
-git clone --depth 1 https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
+git clone https://github.com/KyleRicardo/MentoHUST-OpenWrt-ipk.git package/otherapp/mentohust
 
 # UnblockNeteaseMusic（网易云）
-git clone --depth 1 -b master  https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/unblockneteasemusic
 
 # OpenClash
-git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 # end

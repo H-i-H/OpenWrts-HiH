d2fa3631acceff3292e2df0ae7cc64e3b9754f40
H-i-H <pingjunxiong@outlook.com>
2025-05-24 19:40:53 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -18,7 +18,7 @@ sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' packag
 sed -i 's@.*CYXluq4wUazHjmCDBCqXF*@#&@g' package/lean/default-settings/files/zzz-default-settings
 
 # 加入编译者信息
-# sed -i "s/OpenWrt /HiH build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
+sed -i "s/OpenWrt /HiH build $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" package/lean/default-settings/files/zzz-default-settings
 
 # 修改默认IP为 11.11.11.100（新版）
 sed -i 's/192.168.1.1/11.11.11.100/g' package/base-files/luci2/bin/config_generate

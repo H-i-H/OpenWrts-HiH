606541e029dbaadccced870a8f675b1ed508f651
H-i-H <pingjunxiong@outlook.com>
2025-06-25 20:18:34 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 # 添加锐捷网络 Mentohust
-git clone --depth 1 https://github.com/sbwml/luci-app-mentohust package/mentohust
+git clone https://github.com/sbwml/luci-app-mentohust package/mentohust
 
-# 修改默认IP为11.11.11.10
+# 添加主题 luci-theme-alpha
+git clone https://github.com/derisamedia/luci-theme-alpha.git package/feeds/luci/luci-theme-alpha
+
+# 修改默认IP为11.11.11.1
 sed -i 's/192.168.1.1/11.11.11.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/192.168.1.1/11.11.11.1/g' package/base-files/files/bin/config_generate
 
-# 修改默认主题为luci-theme-design
-sed -i "s/luci-theme-bootstrap/luci-theme-design/g" feeds/luci/collections/luci/Makefile
-sed -i "s/luci-theme-bootstrap/luci-theme-design/g" feeds/luci/collections/luci-light/Makefile
+# 修改默认主题为luci-theme-alpha
+sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci/Makefile
+sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci-light/Makefile
 
 # 修改默认名称为Router
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings

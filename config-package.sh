c01ebaa7d2483648b299e1fd6f00b5475a72244b
H-i-H <pingjunxiong@outlook.com>
2025-06-27 20:29:37 +0800

    Update config-package.sh

--- config-package.sh
+++ config-package.sh
@@ -9,10 +9,6 @@ git clone https://github.com/derisamedia/luci-theme-alpha.git package/feeds/luci
 sed -i 's/192.168.1.1/11.11.11.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/192.168.1.1/11.11.11.1/g' package/base-files/files/bin/config_generate
 
-# 修改默认主题为luci-theme-alpha
-sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci/Makefile
-sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci-light/Makefile
-
 # 修改默认名称为Router
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings
 

076da6d7eb4cfc86dc5f548a588086fceabe992c
H-i-H <pingjunxiong@outlook.com>
2025-06-28 17:58:11 +0800

    Update config-package.sh

--- config-package.sh
+++ config-package.sh
@@ -5,9 +5,13 @@ git clone https://github.com/sbwml/luci-app-mentohust package/mentohust
 # 添加主题 luci-theme-alpha
 git clone https://github.com/derisamedia/luci-theme-alpha.git package/feeds/luci/luci-theme-alpha
 
-# 修改默认IP为11.11.11.1
+# 修改默认IP为192.168.111.1
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
+
+# 修改子网掩码为255.255.0.0
+sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/luci2/bin/config_generate
+sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 # 修改默认名称为Router
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings

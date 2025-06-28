3f0c0ff5cf516e9b326112a09d6a1a3f7cf0efea
H-i-H <pingjunxiong@outlook.com>
2025-06-28 18:21:09 +0800

    Update config-package.sh

--- config-package.sh
+++ config-package.sh
@@ -13,6 +13,10 @@ sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_gener
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/luci2/bin/config_generate
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
+# 修改默认主题为luci-theme-alpha
+sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci/Makefile
+sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci-light/Makefile
+
 # 修改默认名称为Router
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings
 

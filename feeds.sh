95b87124ca26be8837f565b307d1b337a24ec211
H-i-H <pingjunxiong@outlook.com>
2025-08-10 12:10:49 +0800

    Update feeds.sh

--- feeds.sh
+++ feeds.sh
@@ -5,7 +5,7 @@
 echo "src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> "feeds.conf.default"
 
 # 添加nikki
-# echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
 
 # 设置luci版本为openwrt-24.10
 sed -i 's/openwrt-23.05/openwrt-24.10/g' feeds.conf.default

0461f9c37a60f397b1fea4b99bcf64c7340d2c34
H-i-H <pingjunxiong@outlook.com>
2025-08-10 12:10:11 +0800

    Update config-package.sh

--- config-package.sh
+++ config-package.sh
@@ -5,6 +5,9 @@ git clone https://github.com/sbwml/luci-app-mentohust package/mentohust
 # 添加主题 luci-theme-alpha
 git clone https://github.com/derisamedia/luci-theme-alpha.git package/feeds/luci/luci-theme-alpha
 
+# 添加主题 luci-theme-edge
+git clone https://github.com/davinyue/luci-theme-edge.git package/feeds/luci/luci-theme-edge
+
 # 修改默认IP为192.168.111.1
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/luci2/bin/config_generate
 sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_generate
@@ -12,10 +15,6 @@ sed -i 's/192.168.1.1/192.168.111.1/g' package/base-files/files/bin/config_gener
 # 修改子网掩码为255.255.0.0
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/luci2/bin/config_generate
 sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
-
-# 修改默认主题为luci-theme-alpha
-sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci/Makefile
-sed -i "s/luci-theme-bootstrap/luci-theme-alpha/g" feeds/luci/collections/luci-light/Makefile
 
 # 修改默认名称为Router
 sed -i '/uci commit system/i\uci set system.@system[0].hostname='Router'' package/lean/default-settings/files/zzz-default-settings
